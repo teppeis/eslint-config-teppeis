fd28db4b222dd3ab2e41ab55dcb52eba4d2c46f7
teppeis <teppeis@gmail.com>
2016-07-19 04:44:39 +0900

    test(base): testing with mocha

---
+++ test/fixtures/es6.no-const-assign.error.js
@@ -0,0 +1,4 @@
+'use strict';
+
+const a = 1;
+a = 2;
---
+++ test/index.js
@@ -0,0 +1,37 @@
+'use strict';
+
+const glob = require('glob');
+const eslint = require('eslint');
+const CLIEngine = eslint.CLIEngine;
+const generateTest = require('./lib/generateTest');
+
+function verify(configName, opt_configFile) {
+  const configFile = opt_configFile || configName;
+  const paths = glob.sync(`${__dirname}/fixtures/${configName}.*.js`);
+  const engine = new CLIEngine({
+    configFile: `${__dirname}/../${configFile}.js`
+  });
+  return engine.executeOnFiles(paths).results;
+}
+
+function describeVerify(configName) {
+  describe(configName, () => {
+    const results = verify(configName);
+    results.forEach(result => generateTest(result));
+  });
+}
+
+describe('eslint-config-teppeis', () => {
+  describe('base', () => {
+    const results = verify('base', 'index');
+    results.forEach(result => generateTest(result));
+  });
+
+  describeVerify('es6');
+  describeVerify('closure');
+  describeVerify('closure-es6');
+  describeVerify('node');
+  describeVerify('node-es6');
+  describeVerify('node-v4');
+  describeVerify('node-v6');
+});
