fba15ac008c36a6839ccabdd10aa98799b8ac90a
teppeis <teppeis@gmail.com>
2017-08-16 00:52:36 +0900

    feat: revise Node rules

--- node-v4.js
+++ node-v4.js
@@ -9,6 +9,7 @@ module.exports = {
   "rules": {
     "node/no-unsupported-features": [2, {"version": 4}],
     // Not yet in Node v4
+    "prefer-destructuring": 0,
     "prefer-rest-params": 0,
     "prefer-spread": 0,
   },
