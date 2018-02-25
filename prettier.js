851abe79c3ca25733842900da24e7192844ea344
teppeis <teppeis@gmail.com>
2018-02-25 17:50:32 +0900

    feat: change prettier trailingComma from `all` to `es5`

--- prettier.js
+++ prettier.js
@@ -5,11 +5,6 @@ module.exports = {
     "plugin:prettier/recommended",
   ],
   "rules": {
-    "prettier/prettier": ["error", {
-      "printWidth": 100,
-      "singleQuote": true,
-      "trailingComma": "all",
-      "bracketSpacing": false,
-    }],
+    "prettier/prettier": ["error", require("./.prettierrc.json")],
   },
 };
