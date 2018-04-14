87d29ccfcb627b09d8c2dfa82c184f86973d4e7b
teppeis <teppeis@gmail.com>
2018-04-14 15:09:29 +0900

    feat(padding-line-between-statements): disable padding-line-between-statements for import/cjs-import

--- examples/node-v4/node-v4.js
+++ examples/node-v4/node-v4.js
@@ -1,5 +1,10 @@
 'use strict';
 
+/* eslint-disable no-unused-vars */
+
+const promisify = require('util').promisify;
+const readFile = promisify(require('fs').readFile);
+
 // prefer-spread: off
 const nums = [1, 2, 3, 4];
 Math.max.apply(Math, nums);
