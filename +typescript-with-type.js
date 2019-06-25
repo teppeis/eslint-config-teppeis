24b00f74169e18c92bb78ccf729946e6ef9dc0e3
teppeis <teppeis@gmail.com>
2019-06-26 08:47:23 +0900

    feat(typescript): enable @typescript-eslint/no-floating-promise

--- +typescript-with-type.js
+++ +typescript-with-type.js
@@ -9,10 +9,11 @@ module.exports = {
         project: "./tsconfig.json",
       },
       rules: {
+        "@typescript-eslint/no-floating-promises": 2,
         "@typescript-eslint/no-unnecessary-type-assertion": 2,
         "@typescript-eslint/restrict-plus-operands": 2,
         // too opinionated
-        // '@typescript-eslint/promise-function-async': 2,
+        // "@typescript-eslint/promise-function-async": 2,
       },
     },
   ],
