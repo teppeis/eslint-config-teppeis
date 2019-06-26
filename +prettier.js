9f4949bc6422566612d78fba427a5ea38ba78408
teppeis <teppeis@gmail.com>
2019-06-26 09:11:32 +0900

    fix: extend "prettier/unicorn" in +prettier

--- +prettier.js
+++ +prettier.js
@@ -1,7 +1,7 @@
 "use strict";
 
 module.exports = {
-  extends: ["plugin:prettier/recommended", "prettier/@typescript-eslint"],
+  extends: ["plugin:prettier/recommended", "prettier/@typescript-eslint", "prettier/unicorn"],
   rules: {
     "prettier/prettier": ["error", require("./.prettierrc.json"), { usePrettierrc: false }],
   },
