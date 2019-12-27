d7cd43d61a70281f83f9259f5f1b6b62451186ab
teppeis <teppeis@gmail.com>
2019-12-27 17:14:46 +0900

    test: update test for Node v10

--- test/fixtures/typescript.node#no-unsupported-features#node-builtins.fail.ts
+++ test/fixtures/typescript.node#no-unsupported-features#node-builtins.fail.ts
@@ -1,3 +1,3 @@
 // This test checks that eslint-plugin-node is effective.
-// This should be failed because URL is not available in Node v8.
-new URL("https://example.com");
+// This should be failed because TextDecoder is not available in Node v10.
+new TextDecoder("shift_jis");
