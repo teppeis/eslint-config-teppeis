86725af4e2e84220b6bcb9d5e2c64a9a358993f6
teppeis <teppeis@gmail.com>
2021-01-24 13:40:03 +0900

    feat(es2021): enable unicorn/numeric-separators-style

--- examples/es2021/index.js
+++ examples/es2021/index.js
@@ -1,8 +1,10 @@
 /* eslint-disable no-unused-vars */
 'use strict';
 
-// numeric separator
-const num = 1_000;
+// numeric separator (unicorn/numeric-separators-style)
+const num1 = 100000000; // onlyIfContainsSeparator: true
+const num2 = 1_000; // number.minimumDigits: 0
+const num3 = 10_000;
 
 // globals
 new WeakRef();
