7c28f5fafb945e7d890b2e8eb517f6ead49f4df8
teppeis <teppeis@gmail.com>
2021-10-31 22:46:13 +0900

    feat(typescript): change +typescript-with-type rules

--- +typescript-with-type.js
+++ +typescript-with-type.js
@@ -9,12 +9,21 @@ module.exports = {
         project: "./tsconfig.json",
       },
       rules: {
+        "@typescript-eslint/await-thenable": 2,
+        // TS3.8+
+        "@typescript-eslint/consistent-type-exports": 2,
         "@typescript-eslint/no-floating-promises": 2,
+        "@typescript-eslint/no-for-in-array": 2,
+        "@typescript-eslint/no-misused-promises": 2,
+        // only for strictNullChecks enabled
+        "@typescript-eslint/no-unnecessary-condition": 2,
         "@typescript-eslint/no-unnecessary-type-assertion": 2,
-        "@typescript-eslint/restrict-plus-operands": 2,
+        "@typescript-eslint/restrict-plus-operands": [2, { checkCompoundAssignments: true }],
         "@typescript-eslint/restrict-template-expressions": 2,
-        // too opinionated
-        // "@typescript-eslint/promise-function-async": 2,
+        "@typescript-eslint/switch-exhaustiveness-check": 2,
+        // override with jest/unbound-method for testing
+        // https://github.com/jest-community/eslint-plugin-jest/blob/main/docs/rules/unbound-method.md
+        "@typescript-eslint/unbound-method": 2,
       },
     },
   ],
