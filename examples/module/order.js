b376bdbf770e9e12aba5501a68715c19916791bf
teppeis <teppeis@gmail.com>
2022-06-09 00:53:04 +0900

    fix: module example

--- examples/module/order.js
+++ examples/module/order.js
@@ -1,15 +1,12 @@
-/* eslint-disable import/no-duplicates */
-/* eslint-disable import/no-useless-path-segments */
 /* eslint-disable import/no-unresolved */
 /* eslint-disable import/default */
 import eslint from 'eslint';
 import os from 'os';
 import index from '.';
-import parent2 from '../../parent2';
-import parent1 from '../parent';
-import indexSlash from './';
-import sibling2 from './sibling';
-import sibling1 from './sibling/foo';
+import parent2 from '../../parent2.js';
+import parent1 from '../parent.js';
+import sibling2 from './sibling.js';
+import sibling1 from './sibling/foo.js';
 
 // eslint-disable-next-line no-undef
 console.log(os, eslint, sibling2, sibling1, parent1, parent2, index, indexSlash);
