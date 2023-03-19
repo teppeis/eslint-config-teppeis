94ed12a4f2c026a2196bea626412d3d67d793196
Teppei Sato <teppeis@gmail.com>
2023-03-19 15:25:11 +0900

    feat(typescript): enable @typescript-eslint/no-import-type-side-effects rule (#898)

--- +typescript.js
+++ +typescript.js
@@ -37,6 +37,7 @@ module.exports = {
         "@typescript-eslint/consistent-type-assertions": 2,
         "@typescript-eslint/consistent-type-imports": 2,
         "@typescript-eslint/no-duplicate-enum-values": 2,
+        "@typescript-eslint/no-import-type-side-effects": 2,
         "@typescript-eslint/no-useless-empty-export": 2,
         // allow require for power-assert
         // '@typescript-eslint/no-require-imports': 2,
