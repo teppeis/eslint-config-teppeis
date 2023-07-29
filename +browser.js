c96a729a74316fdbdf0341babb2294a10286ece0
teppeis <teppeis@gmail.com>
2023-07-29 15:11:05 +0900

    feat: enable some rules for browser

--- +browser.js
+++ +browser.js
@@ -5,6 +5,12 @@ module.exports = {
     browser: true,
   },
   rules: {
+    "unicorn/prefer-blob-reading-methods": 2,
+    "unicorn/prefer-dom-node-append": 2,
+    "unicorn/prefer-dom-node-dataset": 2,
+    "unicorn/prefer-dom-node-remove": 2,
+    "unicorn/prefer-dom-node-text-content": 2,
+    "unicorn/prefer-modern-dom-apis": 2,
     "unicorn/require-post-message-target-origin": 2,
   },
 };
