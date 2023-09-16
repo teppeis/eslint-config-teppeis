35e5524a5807c683bfbdd2b375b9da5808791605
teppeis <teppeis@gmail.com>
2023-09-16 13:17:07 +0900

    feat: node support .mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@ import { es2022, node, prettier } from "./index.mjs";
 /** @type { import("eslint").Linter.FlatConfig[] } */
 export default [
   ...es2022,
-  node,
+  ...node,
   prettier,
   {
     ignores: ["test/fixtures", "examples"],
