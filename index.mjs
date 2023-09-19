406e6852451bd6a3dab85734ec87b79cc596a052
teppeis <teppeis@gmail.com>
2023-09-19 23:30:02 +0900

    fix: remove eslint-cofig-prettier from prod

--- index.mjs
+++ index.mjs
@@ -1,6 +1,5 @@
-export { default as prettier } from "eslint-config-prettier";
 export { default as mocha } from "./lib/configs/mocha.mjs";
 // TODO: export others from index.mjs
 // TODO: move to lib/index.mjs and change `main`
-// TODO: provide typings d.ts
+// TODO: provide typings d.ts or convert to typescript
 // TODO: init eslint.config.js
