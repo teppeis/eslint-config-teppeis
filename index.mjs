133251d991d7cc5b4f4659d513517cd22e400943
teppeis <teppeis@gmail.com>
2023-09-19 21:16:23 +0900

    chore: add a todo

--- index.mjs
+++ index.mjs
@@ -3,3 +3,4 @@ export { default as mocha } from "./lib/configs/mocha.mjs";
 // TODO: export others from index.mjs
 // TODO: move to lib/index.mjs and change `main`
 // TODO: provide typings d.ts
+// TODO: init eslint.config.js
