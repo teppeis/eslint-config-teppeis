a8206e6f259796b1be43f7000a9a244b69cc3aed
teppeis <teppeis@gmail.com>
2025-04-05 14:28:14 +0900

    refactor(types): cleanup type definitions
    - Remove unnecessary type definitions for packages that include their own types:
      - eslint-plugin-import
      - eslint-plugin-n
      - eslint-plugin-jsdoc
    - Restore and improve type definitions for @eslint-community/eslint-plugin-eslint-comments

--- src/types/@eslint-community/eslint-plugin-eslint-comments.d.ts
+++ src/types/@eslint-community/eslint-plugin-eslint-comments.d.ts
@@ -1,1 +1,22 @@
-declare module "@eslint-community/eslint-plugin-eslint-comments/configs";
+declare module "@eslint-community/eslint-plugin-eslint-comments/configs" {
+  import type { Linter } from "eslint";
+
+  interface Configs {
+    recommended: {
+      name: "@eslint-community/eslint-comments/recommended";
+      plugins: {
+        "@eslint-community/eslint-comments": Linter.Plugin;
+      };
+      rules: {
+        "@eslint-community/eslint-comments/disable-enable-pair": "error";
+        "@eslint-community/eslint-comments/no-aggregating-enable": "error";
+        "@eslint-community/eslint-comments/no-duplicate-disable": "error";
+        "@eslint-community/eslint-comments/no-unlimited-disable": "error";
+        "@eslint-community/eslint-comments/no-unused-enable": "error";
+      };
+    };
+  }
+
+  const configs: Configs;
+  export default configs;
+}
